efab3063563ae1870ea30097473ff90197290caf
logan.chen <loganchen@hotmail.com>
2017-08-30 14:10:35 -0400

    Add showdbs.sql

---
+++ 01SP/01SP/showdbs.sql
@@ -0,0 +1,12 @@
+﻿create procedure [dbo].[showdbs]
+as
+SELECT  @@SERVERNAME AS Server ,
+        name AS DBName ,
+        recovery_model_Desc AS RecoveryModel ,
+        Compatibility_level AS CompatiblityLevel ,
+        create_date ,
+        state_desc
+FROM    sys.databases
+ORDER BY Name
+
+GO
