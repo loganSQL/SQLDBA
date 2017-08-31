bb46b6c924de8214393504511d54103720ca9084
logan.chen <loganchen@hotmail.com>
2017-08-31 12:44:45 -0400

    store procedure to display server properties

---
+++ 01SP/01SP/showserver.sql
@@ -0,0 +1,20 @@
+﻿/*
+	store procedure to display server properties
+*/
+create procedure [dbo].[showserver]
+as
+Select @@SERVERNAME as [Server\Instance],
+-- SQL Server Version
+	@@VERSION as SQLServerVersion,
+-- SQL Server Instance
+	@@ServiceName AS ServiceInstance,
+	SERVERPROPERTY('ProductVersion') AS ProductVersion,
+SERVERPROPERTY('ProductLevel') AS ProductLevel,
+SERVERPROPERTY('Edition') AS Edition,
+SERVERPROPERTY('EngineEdition') AS EngineEdition,
+	SERVERPROPERTY('Collation') AS Collation,
+SERVERPROPERTY('MachineName') AS MachineName,
+SERVERPROPERTY('ProcessID') AS ProcessID,
+SERVERPROPERTY('SqlCharSetName') AS SqlCharSetName;
+
+GO
