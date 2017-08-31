39d40056241fb33e6f9f59c07d55a034476a69be
logan.chen <loganchen@hotmail.com>
2017-08-31 12:47:18 -0400

    store procedure to list all logins with sysadmin role

---
+++ 01SP/01SP/showsysadmin.sql
@@ -0,0 +1,18 @@
+﻿/*
+	store procedure to list all logins with sysadmin role
+*/
+create procedure [dbo].[showsysadmin] as
+SELECT  p.name AS [loginname] ,
+        p.type ,
+        p.type_desc ,
+        p.is_disabled,
+        CONVERT(VARCHAR(10),p.create_date ,101) AS [created],
+        CONVERT(VARCHAR(10),p.modify_date , 101) AS [update]
+FROM    sys.server_principals p
+        JOIN sys.syslogins s ON p.sid = s.sid
+WHERE   p.type_desc IN ('SQL_LOGIN', 'WINDOWS_LOGIN', 'WINDOWS_GROUP')
+        -- Logins that are not process logins
+        AND p.name NOT LIKE '##%'
+        -- Logins that are sysadmins
+        AND s.sysadmin = 1
+GO
