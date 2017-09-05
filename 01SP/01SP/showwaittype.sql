5acc62a54b79a2cb0299f5128753a0ff5766872b
logan.chen <loganchen@hotmail.com>
2017-09-05 15:44:34 -0400

    store procedure to show wait type statistics from sysprocesses

---
+++ 01SP/01SP/showwaittype.sql
@@ -0,0 +1,20 @@
+﻿/*
+	store procedure to show wait type statistics from sysprocesses
+*/
+create procedure [dbo].[showwaittype] as
+select 
+	  lastwaittype
+	, count(*) as '#ofOccurrences'
+	, sum(physical_io) as physicalIO
+	, sum(cpu) as cpu
+	, sum(memusage) as memusage
+from   master.dbo.sysprocesses
+
+group by 
+	  lastwaittype
+
+order by
+	  count(*) desc
+
+
+GO
