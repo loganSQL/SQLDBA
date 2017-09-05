154ea1e8274b8c74bf7d763d191bebeb0ecfc8e7
logan.chen <loganchen@hotmail.com>
2017-09-05 15:31:09 -0400

    store procedure to generate a list of sp_change_users_login for user synchronization in a user database

---
+++ 01SP/01SP/generateusersync.sql
@@ -0,0 +1,17 @@
+﻿/*
+	store procedure to generate a list of sp_change_users_login for user synchronization in a user database
+*/
+
+create procedure [dbo].[generateusersync] as
+
+--select 'EXEC sp_change_users_login ''Update_One'', ['+name+'], ['+name+']' from sysusers 
+
+declare @sqltext varchar(200)
+
+set @sqltext = 'select ''EXEC sp_change_users_login ''''Update_One'''', [''+name+''],[''+name+'']'' from sysusers where uid>4'
+
+print @sqltext
+
+
+
+GO
