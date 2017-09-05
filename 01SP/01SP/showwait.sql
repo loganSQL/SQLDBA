b6bc74abd3b2c4bd3319ce5ab825190b93c28534
logan.chen <loganchen@hotmail.com>
2017-09-05 15:41:00 -0400

    store procedure to show waiting tasks by descending order

---
+++ 01SP/01SP/showwait.sql
@@ -0,0 +1,17 @@
+﻿/*
+	store procedure to show waiting tasks by descending order
+*/
+
+create procedure [dbo].[showwait] as
+SELECT wt.session_id, wt.wait_type
+, er.last_wait_type AS last_wait_type
+, wt.wait_duration_ms
+, wt.blocking_session_id, wt.blocking_exec_context_id, resource_description
+FROM sys.dm_os_waiting_tasks wt
+JOIN sys.dm_exec_sessions es ON wt.session_id = es.session_id
+JOIN sys.dm_exec_requests er ON wt.session_id = er.session_id
+WHERE es.is_user_process = 1
+AND wt.wait_type <> 'SLEEP_TASK'
+ORDER BY wt.wait_duration_ms desc
+
+GO
