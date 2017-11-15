58390f36c865b56960c7d4c1f13d0b0dd08a01cd
logan.chen <loganchen@hotmail.com>
2017-11-15 12:20:42 -0500

    store procedure to show server version

---
+++ 01SP/01SP/showversion.sql
@@ -0,0 +1,22 @@
+﻿/*
+	store procedure to show server version
+*/
+create procedure [dbo].[showversion]
+as
+
+SELECT
+  CASE 
+     WHEN CONVERT(VARCHAR(128), SERVERPROPERTY ('productversion')) like '8%' THEN 'SQL2000'
+     WHEN CONVERT(VARCHAR(128), SERVERPROPERTY ('productversion')) like '9%' THEN 'SQL2005'
+     WHEN CONVERT(VARCHAR(128), SERVERPROPERTY ('productversion')) like '10.0%' THEN 'SQL2008'
+     WHEN CONVERT(VARCHAR(128), SERVERPROPERTY ('productversion')) like '10.5%' THEN 'SQL2008 R2'
+     WHEN CONVERT(VARCHAR(128), SERVERPROPERTY ('productversion')) like '11%' THEN 'SQL2012'
+     WHEN CONVERT(VARCHAR(128), SERVERPROPERTY ('productversion')) like '12%' THEN 'SQL2014'
+     WHEN CONVERT(VARCHAR(128), SERVERPROPERTY ('productversion')) like '13%' THEN 'SQL2016'     
+     ELSE 'unknown'
+  END AS MajorVersion,
+  SERVERPROPERTY('ProductLevel') AS ProductLevel,
+  SERVERPROPERTY('Edition') AS Edition,
+  SERVERPROPERTY('ProductVersion') AS ProductVersion
+
+go
