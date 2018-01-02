1d0ea0627c946d8e2f900f533e7585099772b160
logan.chen <loganchen@hotmail.com>
2018-01-02 12:39:44 -0500

    Restore an encrypted database with Transparent Data Encryption

--- 05SQLSecurity/05SQLSecurity/restoreTDEdatabase.sql
+++ 05SQLSecurity/05SQLSecurity/restoreTDEdatabase.sql
@@ -1,6 +1,7 @@
 ﻿/*
 	Restore an encrypted database with Transparent Data Encryption
 	Ref: https://docs.microsoft.com/en-us/sql/relational-databases/security/encryption/transparent-data-encryption
+	Attn: https://simonmcauliffe.com/technology/tde/
 
 */
 
