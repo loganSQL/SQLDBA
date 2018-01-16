5d64afa69adc50800f85473ef2177e1aad2f63c9
logan.chen <loganchen@hotmail.com>
2018-01-16 15:18:40 -0500

    add compressed_backup_size

--- 01SP/01SP/showbackups.sql
+++ 01SP/01SP/showbackups.sql
@@ -17,6 +17,7 @@ WHEN 'D' THEN 'Database'
 WHEN 'L' THEN 'Log' 
 END AS backup_type, 
 msdb.dbo.backupset.backup_size, 
+msdb.dbo.backupset.compressed_backup_size, 
 msdb.dbo.backupmediafamily.logical_device_name, 
 msdb.dbo.backupmediafamily.physical_device_name, 
 msdb.dbo.backupset.name AS backupset_name, 
@@ -29,5 +30,34 @@ msdb.dbo.backupset.database_name,
 msdb.dbo.backupset.backup_finish_date 
 
 GO
+/*
+create view vwBackup
+as
+SELECT 
+CONVERT(CHAR(100), SERVERPROPERTY('Servername')) AS Server, 
+msdb.dbo.backupset.database_name, 
+msdb.dbo.backupset.backup_start_date, 
+msdb.dbo.backupset.backup_finish_date, 
+msdb.dbo.backupset.expiration_date, 
+CASE msdb..backupset.type 
+WHEN 'D' THEN 'Database' 
+WHEN 'L' THEN 'Log' 
+END AS backup_type, 
+msdb.dbo.backupset.backup_size,
+msdb.dbo.backupset.compressed_backup_size, 
+msdb.dbo.backupmediafamily.logical_device_name, 
+msdb.dbo.backupmediafamily.physical_device_name, 
+msdb.dbo.backupset.name AS backupset_name, 
+msdb.dbo.backupset.description 
+FROM msdb.dbo.backupmediafamily 
+INNER JOIN msdb.dbo.backupset ON msdb.dbo.backupmediafamily.media_set_id = msdb.dbo.backupset.media_set_id 
+WHERE (CONVERT(datetime, msdb.dbo.backupset.backup_start_date, 102) >= GETDATE() - 1) 
+and backupset.type='D'
 
 
+select * from vwBackup
+select cast(round(sum(backup_size) /(1024*1024*1024),2) as numeric(10,2)) as daily_total_original_backup_size_in_GB,
+cast(round(sum(compressed_backup_size) /(1024*1024*1024),2) as numeric(10,2)) as daily_total_after_compressed_backup_size_in_GB
+from vwBackup
+
+*/
