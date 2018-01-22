c51f8081c102cec8d01322a6f7d781711b088020
loganSQL <31350175+loganSQL@users.noreply.github.com>
2018-01-22 15:49:32 -0500

    include size
    include size

--- 01SP/01SP/showdbs.sql
+++ 01SP/01SP/showdbs.sql
@@ -12,5 +12,15 @@ SELECT  @@SERVERNAME AS Server ,
         state_desc
 FROM    sys.databases
 ORDER BY Name
-
+/*
+SELECT  @@SERVERNAME AS Server ,
+        sys.databases.name AS DBName ,
+        sys.databases.create_date ,
+		CONVERT(VARCHAR,SUM(size)*8/1024)+' MB' AS [Total disk space]  
+FROM    sys.databases
+JOIN        sys.master_files  
+ON          sys.databases.database_id=sys.master_files.database_id  
+GROUP BY    sys.databases.name  
+ORDER BY    sys.databases.name
+*/
 GO
