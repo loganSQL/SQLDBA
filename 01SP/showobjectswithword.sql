1cd63c69dacaecb9628cdcc78f35e7cb9d59bd16
logansql <loganchen@hotmail.com>
2018-07-04 12:27:20 -0400

    A store procedure to show all the objects with definition text contains the SearchWord

--- 01SP/showobjectswithword.sql
+++ 01SP/showobjectswithword.sql
@@ -1,10 +1,23 @@
-msdb..showobjectswithword 'Merlin','FNDM'
+/*
+	store procedure to show all objects with definition text contains the SearchWord
+*/
+use msdb
+go
 
-USE FNDM; 
-select DB_NAME() as searchDB,'Merlin' as searchWord;  
-SELECT DISTINCT      o.name AS Object_Name,o.type_desc      
-FROM sys.sql_modules        m           
-INNER JOIN sys.objects  o 
-ON m.object_id=o.object_id      
-WHERE m.definition Like '%Merlin%'      
-ORDER BY 2,1  
+alter procedure [dbo].[showobjectswithword] @SearchWord varchar(255), @searchDB nvarchar(255)
+as
+declare @sqlstr nvarchar(MAX)
+select @sqlstr=N'USE '+@searchDB+'; select DB_NAME() as searchDB,'''+ @SearchWord+''' as searchWord;'
++'
+SELECT DISTINCT
+    o.name AS Object_Name,o.type_desc
+    FROM sys.sql_modules        m 
+        INNER JOIN sys.objects  o ON m.object_id=o.object_id
+    WHERE m.definition Like '+'''%'+@SearchWord+'%'''+'
+    ORDER BY 2,1
+'
+select @sqlstr
+EXECUTE sp_executesql @sqlstr
+GO
+
+exec msdb..[showobjectswithword] 'MySearchWord','MyDB'
