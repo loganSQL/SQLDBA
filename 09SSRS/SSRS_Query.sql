11d5f5fdd7e19e835407d62fb83e43606f22a3a4
LoganSQL <loganchen@hotmail.com>
2019-08-19 15:51:24 -0400

    SSRS Queries to ReportServer meta data

--- 09SSRS/SSRS_Query.sql
+++ 09SSRS/SSRS_Query.sql
@@ -174,3 +174,192 @@ Select Name,Value from ConfigurationInfo
 
 -- roles
 Select RoleName,Description from Roles
+
+
+
+-- oldest time stamp
+SELECT COUNT(*),
+       MIN(ExecutionLog.TimeStart)
+FROM [ReportServer].[dbo].ExecutionLog(NOLOCK);
+
+-- Last 100 execution
+SELECT TOP 100 c.Name,
+               c.[Path],
+               l.InstanceName,
+               l.ReportID,
+               l.UserName,
+               l.RequestType,
+               l.Format,
+               l.Parameters,
+               l.TimeStart,
+               l.TimeEnd,
+               l.TimeDataRetrieval,
+               l.TimeProcessing,
+               l.TimeRendering,
+               l.Source,
+               l.Status,
+               l.ByteCount,
+               l.[RowCount]
+FROM [ReportServer].[dbo].[ExecutionLog](NOLOCK) AS l
+INNER JOIN [ReportServer].[dbo].[Catalog](NOLOCK) AS c ON l.ReportID = C.ItemID
+WHERE c.Type = 2 -- Only show reports 1=folder, 2=Report, 3=Resource, 4=Linked Report, 5=Data Source
+ORDER BY l.TimeStart DESC;
+
+-- list all reports last time time
+SELECT c.Name,
+       c.[Path],
+       COUNT(*) AS TimesRun,
+       MAX(l.TimeStart) AS [LastRun]
+FROM [ReportServer].[dbo].[ExecutionLog](NOLOCK) AS l
+INNER JOIN [ReportServer].[dbo].[Catalog](NOLOCK) AS c ON l.ReportID = C.ItemID
+WHERE c.Type = 2 -- Only show reports 1=folder, 2=Report, 3=Resource, 4=Linked Report, 5=Data Source
+GROUP BY l.ReportId,
+         c.Name,
+         c.[Path];
+
+
+-- List the reports with the number of executions and time last run, including datasources
+SELECT c.Name,
+       c.[Path],
+       COUNT(*) AS TimesRun,
+       MAX(l.TimeStart) AS [LastRun],
+(
+    SELECT SUBSTRING(
+                    (
+                        SELECT CAST(', ' AS VARCHAR(MAX))+CAST(c1.Name AS VARCHAR(MAX))
+                        FROM [ReportServer].[dbo].[Catalog] AS c
+                        INNER JOIN [ReportServer].[dbo].[DataSource] AS d ON c.ItemID = d.ItemID
+                        INNER JOIN [ReportServer].[dbo].[Catalog] c1 ON d.Link = c1.ItemID
+                        WHERE c.Type = 2
+                              AND c.ItemId = l.ReportId
+                        FOR XML PATH('')
+                    ), 3, 10000000) AS list
+) AS DataSources
+FROM [ReportServer].[dbo].[ExecutionLog](NOLOCK) AS l
+INNER JOIN [ReportServer].[dbo].[Catalog](NOLOCK) AS c ON l.ReportID = C.ItemID
+WHERE c.Type = 2 -- Only show reports 1=folder, 2=Report, 3=Resource, 4=Linked Report, 5=Data Source
+GROUP BY l.ReportId,
+         c.Name,
+         c.[Path];
+
+-- List the reports with the number of executions and time last run, including datasources and who has been using the report
+SELECT c.Name,
+       c.[Path],
+       COUNT(*) AS TimesRun,
+       MAX(l.TimeStart) AS [LastRun],
+(
+    SELECT SUBSTRING(
+                    (
+                        SELECT CAST(', ' AS VARCHAR(MAX))+CAST(c1.Name AS VARCHAR(MAX))
+                        FROM [ReportServer].[dbo].[Catalog] AS c
+                        INNER JOIN [ReportServer].[dbo].[DataSource] AS d ON c.ItemID = d.ItemID
+                        INNER JOIN [ReportServer].[dbo].[Catalog] c1 ON d.Link = c1.ItemID
+                        WHERE c.Type = 2
+                              AND c.ItemId = l.ReportId
+                        FOR XML PATH('')
+                    ), 3, 10000000) AS list
+) AS DataSources,
+(
+    SELECT SUBSTRING(
+                    (
+                        SELECT CAST(', ' AS VARCHAR(MAX))+CAST(REPLACE(t.UserName, 'DOMAIN_NAME\', '') AS VARCHAR(MAX))
+                        FROM
+                        (
+                            SELECT TOP 100000 l2.UserName+'('+CAST(COUNT(*) AS VARCHAR(100))+')' AS UserName
+                            FROM [ReportServer].[dbo].[ExecutionLog](NOLOCK) AS l2
+                            WHERE l2.ReportID = l.ReportId
+                            GROUP BY l2.UserName
+                            ORDER BY COUNT(*) DESC
+                        ) AS t
+                        FOR XML PATH('')
+                    ), 3, 10000000)
+) AS UsedBy
+FROM [ReportServer].[dbo].[ExecutionLog](NOLOCK) AS l
+INNER JOIN [ReportServer].[dbo].[Catalog](NOLOCK) AS c ON l.ReportID = C.ItemID
+WHERE c.Type = 2 -- Only show reports 1=folder, 2=Report, 3=Resource, 4=Linked Report, 5=Data Source
+GROUP BY l.ReportId,
+         c.Name,
+         c.[Path];
+
+-- Reports that haven’t been run since the last time the log was cleared.
+SELECT c.Name,
+       c.[Path]
+FROM [ReportServer].[dbo].[ExecutionLog](NOLOCK) AS l
+RIGHT OUTER JOIN [ReportServer].[dbo].[Catalog](NOLOCK) AS c ON l.ReportID = C.ItemID
+WHERE c.Type = 2 -- Only show reports 1=folder, 2=Report, 3=Resource, 4=Linked Report, 5=Data Source
+      AND l.ReportID IS NULL;
+
+-- Reports by count that have been run in the last 24 hours
+SELECT c.Name,
+       c.[Path],
+       COUNT(*) AS TimesRun,
+       MAX(l.TimeStart) AS [LastRun]
+FROM [ReportServer].[dbo].[ExecutionLog](NOLOCK) AS l
+INNER JOIN [ReportServer].[dbo].[Catalog](NOLOCK) AS c ON l.ReportID = C.ItemID
+WHERE c.Type = 2 -- Only show reports 1=folder, 2=Report, 3=Resource, 4=Linked Report, 5=Data Source
+      AND l.TimeStart > GETDATE() - 1
+GROUP BY l.ReportId,
+         c.Name,
+         c.[Path];
+
+
+-- Reports by count that have been run in the last 7 days
+SELECT c.Name,
+       c.[Path],
+       COUNT(*) AS TimesRun,
+       MAX(l.TimeStart) AS [LastRun]
+FROM [ReportServer].[dbo].[ExecutionLog](NOLOCK) AS l
+INNER JOIN [ReportServer].[dbo].[Catalog](NOLOCK) AS c ON l.ReportID = C.ItemID
+WHERE c.Type = 2 -- Only show reports 1=folder, 2=Report, 3=Resource, 4=Linked Report, 5=Data Source
+      AND l.TimeStart > GETDATE() - 7
+GROUP BY l.ReportId,
+         c.Name,
+         c.[Path];
+
+-- reports used by user
+SELECT l.UserName,
+       COUNT(*) AS TimesRun,
+       MAX(l.TimeStart) AS [LastReportRun]
+FROM [ReportServer].[dbo].[ExecutionLog](NOLOCK) AS l
+INNER JOIN [ReportServer].[dbo].[Catalog](NOLOCK) AS c ON l.ReportID = C.ItemID
+GROUP BY l.UserName;
+ 
+-- Reports Datasource
+
+SELECT c.name,
+       c1.Name datasource,
+       c.ItemId
+FROM [ReportServer].[dbo].[Catalog] AS c
+INNER JOIN [ReportServer].[dbo].[DataSource] AS d ON c.ItemID = d.ItemID
+INNER JOIN [ReportServer].[dbo].[Catalog] c1 ON d.Link = c1.ItemID
+WHERE c.Type = 2;
+
+-- 
+SELECT TOP 100 c.Name,
+               c.[Path],
+               AVG(l.TimeDataRetrieval + l.TimeProcessing + l.TimeRendering) / 1000.0 [AverageExecutionTimeSeconds],
+               SUM(l.TimeDataRetrieval + l.TimeProcessing + l.TimeRendering) / 1000.0 [TotalExecutionTimeSeconds],
+               SUM(l.TimeDataRetrieval + l.TimeProcessing + l.TimeRendering) / 1000.0 / 60 [TotalExecutionTimeMinutes],
+               COUNT(*) TimesRun
+FROM [ReportServer].[dbo].[ExecutionLog](NOLOCK) AS l
+INNER JOIN [ReportServer].[dbo].[Catalog](NOLOCK) AS c ON l.ReportID = C.ItemID
+WHERE c.Type = 2 -- Only show reports 1=folder, 2=Report, 3=Resource, 4=Linked Report, 5=Data Source
+GROUP BY c.Name,
+         c.[Path],
+         l.InstanceName,
+         l.ReportID
+HAVING AVG(l.TimeDataRetrieval + l.TimeProcessing + l.TimeRendering) / 1000.0 > 1
+ORDER BY AVG(l.TimeDataRetrieval + l.TimeProcessing + l.TimeRendering) DESC;
+ 
+
+-- Long Running Reports by average execution time
+SELECT c.Name,
+       c.[Path],
+       MAX(l.TimeStart) AS [LastRun]
+FROM [ReportServer].[dbo].[ExecutionLog] AS l WITH (NOLOCK)
+INNER JOIN [ReportServer].[dbo].[Catalog] AS c WITH (NOLOCK) ON l.ReportID = C.ItemID
+WHERE c.Type = 2 -- Only show reports 1=folder, 2=Report, 3=Resource, 4=Linked Report, 5=Data Source
+GROUP BY l.ReportId,
+         c.Name,
+         c.[Path]
+ORDER BY [LastRun] DESC;
