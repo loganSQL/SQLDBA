bd72f89d884010822f5303da74c74bf642ba95b7
LoganSQL <loganchen@hotmail.com>
2019-09-30 13:23:33 -0400

    AG backup maintenance plan

--- 10HADR/05_AG_Maintenance.sql
+++ 10HADR/05_AG_Maintenance.sql
@@ -93,3 +93,28 @@ BEGIN
 END  
 BACKUP DATABASE @DBNAME TO DISK=<disk>  
    WITH COPY_ONLY;
+
+
+   Copy
+
+-- script to generate ad-hoc backups
+DECLARE  @dbname nvarchar(50), @message varchar(1000), @backuppath varchar(100);
+select @backuppath = 'D:\DBBackup\Logansql\'
+DECLARE dbs_cursor CURSOR  
+    FOR SELECT name FROM master..sysdatabases where dbid>4
+
+OPEN dbs_cursor  
+FETCH NEXT FROM dbs_cursor INTO @dbname  
+  
+WHILE @@FETCH_STATUS = 0  
+BEGIN  
+	PRINT ' '  
+    --SELECT @message = 'DBNAME = ' + @DBNAME  
+    --PRINT @message  
+	SELECT @message = 'BACKUP DATABASE '+@DBNAME+' TO DISK = '''+@backuppath+@DBNAME+'.bak'' WITH FORMAT, COPY_ONLY'
+	PRINT @message  
+    FETCH NEXT FROM dbs_cursor INTO @dbname  
+END  
+  
+CLOSE dbs_cursor  
+DEALLOCATE dbs_cursor  
