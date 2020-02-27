28e337b60659826a92f8cdf153f63a6c3f5a8b91
LoganSQL <loganchen@hotmail.com>
2020-02-27 13:27:10 -0500

    script for database mirroring

---
+++ 10HADR/00_DB_Mirroring.sql
@@ -0,0 +1,36 @@
+/*
+T-SQL Script to create a SQL Mirroring:-
+The first thing you need to do when setting up Database Mirroring is perform a full backup followed by a transaction log backup on the principal server.  You then must restore these to the mirror server using the WITH NORECOVERY option of the RESTORE command.  
+*/
+-- 0. Restore the backup from Principal on Mirror with norecovery
+USE [master]
+RESTORE DATABASE DatabaseName
+FROM  DISK = N'F:\DBBackup\DatabaseName_Mirror.bak' 
+WITH  FILE = 1,  
+MOVE N'DatabaseName' TO N'E:\Databases\DatabaseName.mdf',  
+MOVE N'DatabaseName_log' TO N'E:\Databases\DatabaseName_log.ldf',  
+NORECOVERY,  NOUNLOAD,  REPLACE,  STATS = 5
+-- restore log on Mirror with norecovery
+RESTORE LOG DatabaseNameFROM  DISK = N'F:\DBBackup\DatabaseName_mirror' 
+WITH  FILE = 2,  
+NORECOVERY,  NOUNLOAD,  STATS = 5
+
+GO
+
+
+
+
+-- 1. Create endpoints on both servers
+CREATE ENDPOINT EndPointName
+STATE=STARTED AS TCP(LISTENER_PORT = PortNumber, LISTENER_IP = ALL)
+FOR DATA_MIRRORING(ROLE = PARTNER, AUTHENTICATION = WINDOWS NEGOTIATE, ENCRYPTION = REQUIRED ALGORITHM RC4)
+-- 2. Set partner and setup job on mirror server, default port 5022
+ALTER DATABASE DatabaseName SET PARTNER = N'TCP://PrincipalServer:PortNumber'
+EXEC sys.sp_dbmmonitoraddmonitoring -- default is 1 minute
+-- 3. Set partner, set asynchronous mode, and setup job on principal server
+ALTER DATABASE DatabaseName SET PARTNER = N'TCP://MirrorServer:PortNumber'
+ALTER DATABASE DatabaseName SET SAFETY OFF
+EXEC sys.sp_dbmmonitoraddmonitoring -- default is 1 minute
+-- 4. FAILOVER
+ALTER DATABASE DatabaseName SET PARTNER FAILOVER
+ALTER DATABASE DatabaseName SET PARTNER FORCE_SERVICE_ALLOW_DATA_LOSS
