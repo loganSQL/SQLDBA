7e397245fa45ccd7d3f68749759618b1c014616a
LoganSQL <loganchen@hotmail.com>
2020-05-25 15:11:21 -0400

    Script for Windows Logins

--- 05SQLSecurity/SQLLoginCreation.sql
+++ 05SQLSecurity/SQLLoginCreation.sql
@@ -17,7 +17,7 @@ SELECT 'IF(SUSER_ID('+QUOTENAME(SP.name,'''')+') IS NULL) BEGIN CREATE LOGIN '+Q
 /*-- generate script like
 IF(SUSER_ID('TestUser') IS NOT NULL) BEGIN ALTER LOGIN [TestUser] WITH PASSWORD = 0x020026FB1D74A372FC6A0C91EA2E98ABA887943D99F538DAD93BFB1946D8746E94DE3E112355CE5BC65F1A965419531624F586A060C227B0EC1AFBFAAACF3CF1C49EF2AC6405 HASHED;/*SQL_LOGIN*/ END;
 */
-SELECT 'IF(SUSER_ID('+QUOTENAME(SP.name,'''')+') IS NULL) BEGIN CREATE LOGIN '+QUOTENAME(SP.name)+
+SELECT 'IF(SUSER_ID('+QUOTENAME(SP.name,'''')+') IS NOT NULL) BEGIN ALTER LOGIN '+QUOTENAME(SP.name)+
        CASE WHEN SP.type_desc = 'SQL_LOGIN'
             THEN ' WITH PASSWORD = '+CONVERT(NVARCHAR(MAX),SL.password_hash,1)+' HASHED'
             ELSE ' FROM WINDOWS'
@@ -40,4 +40,15 @@ WHERE SP.type_desc ='SQL_LOGIN'
    AND SP.name NOT IN ('SA')
 Order by SP.name
 
-
+-- generate swindow login creation script
+-- simple format
+SELECT 'IF(SUSER_ID('+QUOTENAME(SP.name,'''')+') IS NULL) BEGIN CREATE LOGIN '+QUOTENAME(SP.name)+
+       CASE WHEN SP.type_desc = 'SQL_LOGIN'
+            THEN ' WITH PASSWORD = '+CONVERT(NVARCHAR(MAX),SL.password_hash,1)+' HASHED'
+            ELSE ' FROM WINDOWS'
+       END + ';/*'+SP.type_desc+'*/ END;' 
+       COLLATE SQL_Latin1_General_CP1_CI_AS
+  FROM sys.server_principals AS SP
+  LEFT JOIN sys.sql_logins AS SL
+    ON SP.principal_id = SL.principal_id
+ WHERE SP.type_desc = 'WINDOWS_LOGIN' 
